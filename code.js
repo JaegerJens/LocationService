025a53f9d5dafb5eb6e44192f35caa26990cb5d0
Jens Jaeger <jaegerj204@googlemail.com>
2018-02-11 13:59:31 +0100

    get geolocation as promise

--- code.js
+++ code.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+function getCurrentLocation() {
+    return new Promise((resolve, reject) => {
+        if (!("geolocation" in navigator)) {
+            console.error('no geolocation support');
+            reject('no geolocation support');
+        }
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
+            resolve(position);
+        }, error => {
+            console.error(error);
+            reject(error);
+        });
+    });
+}
